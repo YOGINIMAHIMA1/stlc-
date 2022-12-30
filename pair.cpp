5e7f8b7fec8bbc9b7c704916b16d3b6b35dc53ca
Mahima Bhardwaj <90374311+YOGINIMAHIMA1@users.noreply.github.com>
2022-12-30 21:06:55 +0530

    Update pair.cpp

--- pair.cpp
+++ pair.cpp
@@ -3,8 +3,11 @@ using namespace std;
 int main()
 {
  pair<int,int>p={1,3};
- for(auto i:p)
- {
-   cout<<i<<endl;
-   }
+ 
+   cout<<p.first<<" ,"<<p.second<<endl;
+   pair<pair<int,char>,string>p1={{1,'a'},"xyz"};
+   cout<<p1.first.first<<" ,"<<p1.first.second<<" ,"<<p1.second<<endl;
+   pair<pair<int,char>,pair<string,string>>p2={{1,'a'},{"xxx","yyy"}};
+   cout<<p2.first.first<<" "<<p2.first.second<<" "<<p2.second.first<<" "<<p2.second.second<<endl;
+   
    }
